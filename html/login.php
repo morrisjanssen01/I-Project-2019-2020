e5f685f4c7c5ac79955c4fde38163e45468c2010
nide57 <niels1575@gmail.com>
2019-11-22 12:00:49 +0100

    aanpassing haakjes

--- html/login.php
+++ html/login.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link type="text/css" rel="stylesheet" href="node_modules/materialize-css/dist/css/materialize.css"
+        <link type="text/css" rel="stylesheet" href="node_modules/materialize-css/dist/css/materialize.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
