b25ef787d3f11ba61a99883a86359d4f21421b19
TheEdgyDylan <dylanvanduijnhoven@gmail.com>
2019-11-22 11:57:27 +0100

    te testen login in script
    dit moet getest worden en de database connecctie moet nog gemaakt worden

---
+++ helpers/loginScript.php
@@ -0,0 +1,42 @@
+<?php
+
+    function Login() {
+
+        $username = $_GET[username];
+        $password = $_GET["password"];
+
+        /* stuur de gebruiker naar de homepage als ze een vak niet invullen */
+        if(empty($username) || empty($password)){
+            header('location: index.php');
+            exit;
+
+        }
+        else{
+
+            $sql = 'SELECT * FROM gebruikers where gebruikersnaam = :username';
+            $query = preparedQuery($dbh , $sql [$username]); /* dbh connectie moet nog geschreven worden */
+            $result = $query->fetch(PDO::FETCH_ASSOC);
+            $accountExist = (is_array($result));
+
+            if($accountExist){
+
+                $passwordCorrect = password_verify($password, $result('wachtwoord'));
+
+                if($passwordCorrect){
+
+                    session_start();
+
+                    $_SESSION["username"] = $result['gebruikersnaam']
+
+                }
+
+            }
+            else {
+
+                header('location: login.php');
+
+            }
+
+
+        }
+    }
