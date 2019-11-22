2498f81660f66d12ae0691330cc3d7c416f4e23b
morrisjanssen01 <desktopmorris@outlook.com>
2019-11-22 11:45:10 +0100

    /

--- html/login.php
+++ html/login.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link type="text/css" rel="stylesheet" href="node_modules/materialize-css/dist/css/materialize.css"
+        <link type="text/css" rel="stylesheet" href="../node_modules/materialize-css/dist/css/materialize.css"
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
@@ -53,7 +53,7 @@
             </div>
         </section>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-        <script type="text/javascript" src="node_modules/materialize-css/dist/js/materialize.js"></script>
+        <script type="text/javascript" src="../node_modules/materialize-css/dist/js/materialize.js"></script>
         <script>
             $(document).ready(function(){
                 $('.sidenav').sidenav();
