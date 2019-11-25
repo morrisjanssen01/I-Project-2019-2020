ec06111f43ca678d6d4f1b4801ac9e6aec3ade03
RichieRamz18 <57618770+RichieRamz18@users.noreply.github.com>
2019-11-25 10:18:13 +0100

    /

--- helpers/ConnectieDatabaseScript.php
+++ helpers/ConnectieDatabaseScript.php
@@ -6,5 +6,15 @@ $username = 'username'
 $password = 'password'
 
 
+try {
+ $dbh = new PDO ("mysql:host=$hostname;Database=$dbname";$user, $pw);
+$dbh->setAttribute(PDO: :ATTR_ERRMODE, PDO: :ERRMODE_EXCEPTION);
+}
 
+catch (PDOException $exception){
+    echo "Er ging iets mis met de database. <br>"
+    echo "De melding is {$exception->getMessage()}<br><br>";
+}
+
+$data = $dbh->query("SELECT * FROM voornamen");
 ?>
