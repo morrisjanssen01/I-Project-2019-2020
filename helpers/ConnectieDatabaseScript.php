7d85d44b1cc9796fd5b0929d9617022a6e87dbc0
morrisjanssen01 <desktopmorris@outlook.com>
2019-11-25 10:46:44 +0100

    finalizing connectieDatabaseScript

--- helpers/ConnectieDatabaseScript.php
+++ helpers/ConnectieDatabaseScript.php
@@ -1,20 +1,19 @@
 <?php 
 
-$hostname = 'localhost'
-$dbname = 'EenmaalAndermaal'
-$username = 'username'
-$password = 'password'
+$hostname = 'localhost';
+$dbname = 'EenmaalAndermaal';
+$username = 'username';
+$password = 'password';
 
 
 try {
- $dbh = new PDO ("mysql:host=$hostname;dbname=$dbname";$user, $pw);
-$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$password");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-
 catch (PDOException $exception){
-    echo "Er ging iets mis met de database. <br>"
+    echo "Er ging iets mis met de database. <br>";
     echo "De melding is {$exception->getMessage()}<br><br>";
-}
+};
 
 $data = $dbh->query("SELECT * FROM voornamen");
 ?>
