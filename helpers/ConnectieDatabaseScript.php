c8038dd94b0c5eb80ef5071bf3f3c6a8aa51b19d
RichieRamz18 <57618770+RichieRamz18@users.noreply.github.com>
2019-11-26 11:22:49 +0100

    functie gemaakt voor de connectie

--- helpers/ConnectieDatabaseScript.php
+++ helpers/ConnectieDatabaseScript.php
@@ -5,7 +5,7 @@ $dbname = 'EenmaalAndermaal';
 $username = 'username';
 $password = 'password';
 
-
+function ConnectionDatabase($hostname, $dbname, $username, $password) {}
 try {
     $dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;ConnectionPooling=0", "$username", "$password");
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -13,6 +13,7 @@ try {
 catch (PDOException $exception){
     echo "Er ging iets mis met de database. <br>";
     echo "De melding is {$exception->getMessage()}<br><br>";
+}
 };
 
 ?>
--- helpers/RegistrerenScript.php
+++ helpers/RegistrerenScript.php
@@ -6,11 +6,14 @@
 
 /* deze functie checkt of de wachtwoorden overeenkomen */
 
+/* deze functie geeft de ingevulde waarden bij gebruikersnaam en wachtwoord mee aan de variabelen */
+function 
+
 
 /*  Deze functie checkt of de gebruikersnaam nog niet bestaat */
 function CheckUsername() {
 	if(isset($_POST["username"])){
-		
+
 	}
 }
 
