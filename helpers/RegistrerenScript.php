3ae06ab4af7037f15b46b76be5c2ccb88c3b0037
RichieRamz18 <57618770+RichieRamz18@users.noreply.github.com>
2019-11-26 13:06:28 +0100

    /

--- helpers/RegistrerenScript.php
+++ helpers/RegistrerenScript.php
@@ -1,13 +1,25 @@
 <?php
 
+$username = $_POST(["username"]);
+
+
+
 /* deze functie checkt of er een gebruikernaam en wachtwoord zijn ingevuld */
 
 
 
 /* deze functie checkt of de wachtwoorden overeenkomen */
+function CheckIfDouble() {
+
+}
+
+
+/* deze functie geeft een melding wanneer niet alle velden zijn ingevuld */
+
+
 
 /* deze functie geeft de ingevulde waarden bij gebruikersnaam en wachtwoord mee aan de variabelen */
-function 
+
 
 
 /*  Deze functie checkt of de gebruikersnaam nog niet bestaat */
