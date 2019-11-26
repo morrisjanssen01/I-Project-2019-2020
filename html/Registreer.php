0e0863a82e13afa52620040b26a9cb8376abe581
nide57 <niels1575@gmail.com>
2019-11-26 11:15:23 +0100

    Pagina toegevoegd  + vragen knop verplaatst

--- html/Registreer.php
+++ html/Registreer.php
@@ -6,32 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
-        <?php include '../includes/header.php'; ?>
-     <!-- maakt knop -->
-     <a class="waves-effect waves-light btn modal-trigger"  href="#modal1" >Vragen?</a> 
-
-    <!-- structuur vraag knop --> 
-      <div id="modal1" class="modal">
-       <div class="modal-content">
-       <h4>Registreer handleiding</h4> 
-       <p> 
-       <ol>
-          <li>Vul uw e-mailadres in.</li>
-          <li>Ga naar uw mailbox en klik op de link die in de mail staat. Daarna ga naar stap 4 (indien u de mail niet heeft ontvangen, ga naar stap 3)</li>
-          <li>Indien u de mail niet heeft ontvangen druk op de e-mail niet ontvangenknop daarna herhaal stap 2</li> 
-          <li>Kies een unieke gebruikersnaam.</li>
-          <li>Kies een goed wachtwoord.</li>
-          <li>Onthou het wachtwoord en herhaal het wachtwoord dat u gekozen heeft.</li>
-          <li>Lees de voorwaarden die verbonden zijn aan het registreren.</li> 
-          <li>Wanneer u met de voorwaarden akkoord gaat, vink de akkoordknop onder aan het scherm aan.</li> 
-          <li>Druk op de Registreer-knop. </li>
-        </ol> 
-            </p>
-       </div>
-        <div class="modal-footer"> 
-             <a href="#!" class="modal-close waves-effect waves-green btn-flat">Terug naar de Registreer-pagina</a>
-          </div>
-        </div>
+        <?php include '../includes/header.php'; ?> 
         <!-- Registreer stuk van de site --> 
         <section class="section container center" id="loginForm">
             <div class="row">
@@ -39,6 +14,8 @@
                     <div class="card">
                         <div class="card-action warmSand darken-2 white-text">
                             <h3 class="center">Registreren</h3>
+                            <!-- maakt de vragenknop aan --> 
+                            <a class="waves-effect waves-light btn modal-trigger"  href="#modal1" > Vragen?</a> 
                         </div>
                         <div class="card-content">
                           <form action="../helpers/RegistrerenScript.php" method="post">
@@ -76,6 +53,28 @@
                 </div>
             </div>
         </section>
+          <!-- structuur vraag knop --> 
+          <div id="modal1" class="modal">
+                     <div class="modal-content">
+                       <h4>Registreer handleiding</h4> 
+                        <p> 
+                          <ol>
+                            <li>Vul uw e-mailadres in.</li>
+                             <li>Ga naar uw mailbox en klik op de link die in de mail staat. Daarna ga naar stap 4 (indien u de mail niet heeft ontvangen, ga naar stap 3)</li>
+                             <li>Indien u de mail niet heeft ontvangen druk op de e-mail niet ontvangenknop daarna herhaal stap 2</li> 
+                            <li>Kies een unieke gebruikersnaam.</li>
+                            <li>Kies een goed wachtwoord.</li>
+                            <li>Onthou het wachitwoord en herhaal het wachtwoord dat u gekozen heeft.</li>
+                            <li>Lees de voorwaarden die verbonden zijn aan het registreren.</li> 
+                            <li>Wanneer u met de voorwaarden akkoord gaat, vink de akkoordknop onder aan het scherm aan.</li> 
+                            <li>Druk op de Registreer-knop. </li>
+                          </ol> 
+                               </p>
+                            </div>
+                              <div class="modal-footer"> 
+                              <a href="#!" class="modal-close waves-effect waves-green btn-flat">Terug naar de Registreer-pagina</a>
+                           </div>
+                         </div>
         <?php include '../includes/footer.php'; ?>
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="../node_modules/materialize-css/dist/js/materialize.js"></script>
