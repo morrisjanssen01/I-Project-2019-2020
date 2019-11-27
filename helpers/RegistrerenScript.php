c01a12cef9d45718d6fe0b84c7c8124246cb4e66
RichieRamz18 <57618770+RichieRamz18@users.noreply.github.com>
2019-11-27 23:28:42 +0100

    antiSQLinjection

--- helpers/RegistrerenScript.php
+++ helpers/RegistrerenScript.php
@@ -1,49 +1,44 @@
 <?php
 
 require("ConnectieDatabaseScript.php");
+require("antiSQLinjection.php");
 
-$username = $_POST(["username"]);
-$password = $_POST(["password"]);
-$repeatpassword = $_POST(["repeat password"]);
-$mailaddres = $_POST(["mailadress"]);
-
-
-function CheckRegistration() {
-
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
+	$_SESSION["postedData"]=$_POST;
 }
 
-/* checkt of de waarden die bij 'wachtwoord' en 'bevestig wachtwoord' met elkaar overeenkomen */
-function checkPassword(){
-if($password == $repeatpassword){
-	submitRegistration();
-}
-	else{
-		echo "Wachtwoorden komen niet overeen";
-	}
-}
-
-/* Deze functie hasht het wachtwoord */
-function hashPassword() {
-	
-}
-
+/* Geeft de ingevulde waarden bij gebruikersnaam en wachtwoord mee aan de variabelen */
+if(isset($_POST["submit"])){
+	$username = $dbh->real_escape_string($_POST(["username"]));
+	$password = $dbh->real_escape_string($_POST(["password"]));
+	$repeatpassword = $dbh->real_escape_string($_POST(["Rpassword"]));
+	$mailaddres = $dbh->real_escape_string($_POST(["mailadress"]));
 
 /* Geeft een melding zodra niet alle velden zijn ingevuld */
-function 
+
 if(empty($username) || empty($password) || empty($repeatpassword) || empty($mailadress) {
 	echo "Nog niet alle velden zijn ingevuld";
 	exit;
+
+	else if(specialCharacters($_POST)){
+
+		redirect('index');
+	}
 })
 
+/* Checkt of wachtwoorden overeenkomen. Wanneer de wachtwoorden overeenkomen, wordt het wachtwoord gehasht */
 
-/* deze functie geeft de ingevulde waarden bij gebruikersnaam en wachtwoord mee aan de variabelen */
-if(isset($_POST["submit"])){
+	if($password != $repeatpassword){
+		echo "wachtwoorden komen niet overeen";
+	}
+	else {
+	$hashedpassword = password_hash($password, PASSWORD_DEFAULT); 
 	$dbh = "INSERT INTO gebruikers (gebruikersnaam, Emailadres, wachtwoord)
-	VALUES ($username, $mailadress, $password)";
+	VALUES ($username, $mailadress, $hashedpassword)";
+	echo "U bent geregistreerd!"
+	}
 
+	
 
-/*  Deze functie checkt of de gebruikersnaam nog niet bestaat */
-	if 
-}
 
 ?>
---
+++ html/Registreer.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="../node_modules/materialize-css/dist/css/materialize.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <?php include '../includes/header.php'; ?> 
+        <!-- Registreer stuk van de site --> 
+        <section class="section container center" id="loginForm">
+            <div class="row">
+                <div class="col s12 l6 offset-l3">
+                    <div class="card">
+                        <div class="card-action warmSand darken-2 white-text">
+                            <h3 class="center">Registreren</h3>
+                            <!-- maakt de vragenknop aan (verander brown om de kleur te veranderen)-->   
+                            <a class="waves-effect waves-light brown btn modal-trigger"  href="#modal1" > Vragen?</a> 
+                        </div>
+                        <div class="card-content">
+                          <form action="../helpers/RegistrerenScript.php" method="post">
+                            <div class="form-field">
+                                <label for="mailadress">E-mail</label>
+                                <input type="text" name= "mailadress" id="mailadress"> 
+                            </div><br>
+                        <div class="card-content">
+                            <div class="form-field">
+                                <label for="username">Gebruikersnaam</label>
+                                <input type="text" name="username" id="username">
+                            </div><br>
+                            <div class="form-field">
+                                <label for="password">Wachtwoord</label>
+                                <input type="password" name ="password" id="password">
+                            </div><br>
+                            <div class="form-field">
+                                <label for="repeat password">Bevestig Wachtwoord</label>
+                                <input type="password" name="Rpassword" id="Rpassword">
+                            <div class="form-field">
+                            </div><br>
+                                <label>
+                                <input type="checkbox" id="remember"/>
+                                <span>Akkoord met Voorwaarden</span>
+                                </label>
+                            </div><br>
+                            <div class="form-field">
+                                <button class="btn-large warmSand darken-2" name="submit" id="submit">Registreer</button>
+                            </div><br>
+                            <div class="form-field">
+                            <button class="btn-large warmSand darken-2">e-mail niet ontvangen</button>
+                            </div><br> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+          <!-- structuur vraag knop --> 
+          <div id="modal1" class="modal">
+                     <div class="modal-content">
+                       <h4>Registreer handleiding</h4> 
+                        <p> 
+                          <ol>
+                            <li>Vul uw e-mailadres in.</li>
+                            <li>Ga naar uw mailbox en klik op de link die in de mail staat. Daarna ga naar stap 4 (indien u de mail niet heeft ontvangen, ga naar stap 3)</li>
+                            <li>Indien u de mail niet heeft ontvangen druk op de e-mail niet ontvangenknop daarna herhaal stap 2</li> 
+                            <li>Kies een unieke gebruikersnaam.</li>
+                            <li>Kies een goed wachtwoord.</li>
+                            <li>Onthou het wachitwoord en herhaal het wachtwoord dat u gekozen heeft.</li>
+                            <li>Lees de voorwaarden die verbonden zijn aan het registreren.</li> 
+                            <li>Wanneer u met de voorwaarden akkoord gaat, vink de akkoordknop onder aan het scherm aan.</li> 
+                            <li>Druk op de Registreer-knop. </li>
+                          </ol> 
+                               </p>
+                            </div>
+                              <div class="modal-footer"> 
+                              <a href="#!" class="modal-close waves-effect waves-green btn-flat">Terug naar de Registreer-pagina</a>
+                           </div>
+                         </div>
+        <?php include '../includes/footer.php'; ?>
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="../node_modules/materialize-css/dist/js/materialize.js"></script>
+        <script>
+            $(document).ready(function(){
+                $('.sidenav').sidenav();
+            });
+            // Vragen knop
+            $(document).ready(function(){
+               $('.modal').modal();
+            });
+      
+        </script>
+        
+    </body>
+</html>
+<!-- 2de knop maken die een 2de mail stuurd doormiddel van een knop e-mail niet ontvangen of een knop E-mail binnen 5 min niet ontvangen
