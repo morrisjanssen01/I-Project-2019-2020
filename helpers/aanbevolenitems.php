a01be6dd94a4e0fe5a8ab29bbdb8fd2c95061ead
TheEdgyDylan <dylanvanduijnhoven@gmail.com>
2019-11-27 01:01:16 +0100

    nog meer aanpassingen
    moet nog wel getest worden maar dat doe ik morgen nu lekker slapen

--- helpers/aanbevolenitems.php
+++ helpers/aanbevolenitems.php
@@ -3,6 +3,7 @@
   require("ConnectieDatabaseScript.php");
 
    /*deze pagina is voorlopig voor test purposes en testen. Ik ga nog met jullie implementeren op de website maar daarvoor moet iedereen er zijn! ~dylan 26-11 */
+   /*deze functie moet worden aangeroepen binnen de container en de row van de aanbevolen veilingen */
 
     Function getDecayingItems(){
 
@@ -14,13 +15,16 @@
          }
          foreach($datas as $data){ 
             
-            echo '<div class="col 4><h1 class="center">'. $data["titel"] . $data["naam"] . $data["datum"]. $data["beschrijving"].$data["looptijdEindeTijdstip"].'</h1></div>';
-
+            echo '<div class="col 4>
+                  <h1 class="center">'. $data["titel"]. '</h1>
+                    <img src="" class="responsive">
+                    <p>'. $data["beschrijving"].'</p>
+                    <h3 class="center">'.$data["looptijdEindeTijdstip"].'
+                  </div>';
+            /* Hier moet een img komen ook ergens */
 
           } 
     }
-
-/* hier komt ga ik de sql uitwerken samen met iemand*/
 
   
 ?>
