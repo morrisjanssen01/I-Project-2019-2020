ac11f17719bbf5cd28d2cd48f6494d0910ff018d
lars092000 <larsboxmeer@gmail.com>
2019-11-29 15:32:50 +0100

    Update RegistrerenScript.php

--- helpers/RegistrerenScript.php
+++ helpers/RegistrerenScript.php
@@ -6,11 +6,12 @@ if ($_SERVER["REQUEST_METHOD"]=="POST") {
 }
 
 /* Geeft de ingevulde waarden bij gebruikersnaam en wachtwoord mee aan de variabelen */
+/* zet het mailadres in een sessie en unset deze nadat je hem in de database hebt gestopt.	*/
 if(isset($_POST["submit"])){
 	$username = $dbh->real_escape_string($_POST(["username"]));
 	$password = $dbh->real_escape_string($_POST(["password"]));
 	$repeatpassword = $dbh->real_escape_string($_POST(["Rpassword"]));
-	$mailaddres = $dbh->real_escape_string($_POST(["mailadress"]));
+	$mailaddress = $dbh->real_escape_string($_POST(["mailadress"]));
 /* Geeft een melding zodra niet alle velden zijn ingevuld */
 try {
 if(empty($username) || empty($password) || empty($repeatpassword) || empty($mailadress)) {
