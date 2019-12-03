ad82b21ee3ff724e8e39ee74d488c04c3b6dc8dc
lars092000 <larsboxmeer@gmail.com>
2019-12-03 19:50:02 +0100

    css-fix login

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
     <body>
-        <div class="wrapper">
+        <div class="index-wrapper">
             <div class="box header">
                 <?php include 'includes/header.php';?>
             </div>
