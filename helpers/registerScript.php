8880c35d6c4c0cf21c5dcdb5e136533ca6fc05d8
TheEdgyDylan <dylanvanduijnhoven@gmail.com>
2019-12-03 11:09:03 +0100

    update registerscript

--- helpers/registerScript.php
+++ helpers/registerScript.php
@@ -1,9 +1,13 @@
 <?php
 require("connectionDatabaseScript.php");
 require("antiSQLinjectionscript.php");
+require("redirect.php");
 
 error_reporting(E_ERROR | E_PARSE);
-
+if(!isset($_SESSION["mailadress"])){
+	redirect("register email");
+	exit();
+}
 if ($_SERVER["REQUEST_METHOD"]=="POST") {
 	$_SESSION["postedData"]=$_POST;
 }
@@ -14,7 +18,7 @@ if(isset($_POST["username"])) {
 	$sql = $dbh->prepare("SELECT * FROM gebruikers WHERE gebruikersnaam = ?");
 	$query = $sql->execute(array([$username]));
 	$users = $query->fetchAll(pdo::FETCH_ASSOC);
-	foreach ($users as $user){
+	if(isarray($user)){
 		
 	};
 	if(sqlsrv_nums_row($query) == 0){
@@ -24,12 +28,13 @@ if(isset($_POST["username"])) {
 		}
   }
 }	
-else {
+/*else {
 	if(isset($_POST["gebruikersnaam"])){
     $existing = existing_customer();
 	echo'Deze gebruikersnaam staat al geregistreerd!';
+	exit();
 	}	
-}
+ } */
 
 /* Geeft de ingevulde waarden bij gebruikersnaam en wachtwoord mee aan de variabelen */
 /* zet het mailadres in een sessie en unset deze nadat je hem in de database hebt gestopt.	*/
@@ -38,12 +43,22 @@ if(isset($_POST["submit"])){
 	$password = $dbh->real_escape_string($_POST(["password"]));
 	$repeatpassword = $dbh->real_escape_string($_POST(["Rpassword"]));
 	$mailaddress = $dbh->real_escape_string($_POST(["mailadress"]));
+	$firstname = $dbh->real_escape_string($_POST["firstName"]));
+	$lastName = $dbh->real_escape_string($_POST["lastName"]));
+	$adress1 = $dbh->real_escape_string($_POST["adress1"]));
+	$adress2 = $dbh->real_escape_string($_POST["adress2"]));
+	$postalCode = $dbh->real_escape_string($_POST["postalCode"]));
+	$city = $dbh->real_escape_string($_POST["city"]));
+	$land = $dbh->real_escape_string($_POST["land"]));
+	$birthDate = $dbh->real_escape_string($_POST["birthDate"]));
+	$questionList = $dbh->real_escape_string($_POST["questionList"]));
+	$answer = $dbh->real_escape_string($_POST["answer"]));
 /* Geeft een melding zodra niet alle velden zijn ingevuld */
 try {
 if(empty($username) || empty($password) || empty($repeatpassword) || empty($mailadress)) {
 	echo "Nog niet alle velden zijn ingevuld";
 }
-elseif(specialCharacters($_POST)){
+else if(specialCharacters($_POST)){
 		redirect('index');
 }
 }
@@ -58,8 +73,8 @@ catch (PDOException $exception){
 	}
 	else {
 	$hashedpassword = password_hash($password, PASSWORD_DEFAULT); 
-	$dbh = "INSERT INTO gebruikers (gebruikersnaam, Emailadres, wachtwoord, voornaam, achternaam, adresregel1, adresregel2, postcode, plaatsnaam, landnaam, geboortedag, emailadres, vraagnummer, antwoordtekst)
-	VALUES ($username, $mailadress, $hashedpassword)";
+	$dbh = "INSERT INTO gebruikers (gebruikersnaam, Emailadres, wachtwoord, voornaam, achternaam, adresregel1, adresregel2, postcode, plaatsnaam, landnaam, geboortedag, vraagnummer, antwoordtekst)
+			VALUES ($username, $mailadress, $hashedpassword, $firstname, $lastName, $adress1, $adress2, $postalCode, $city, $land, $birthDate, $questionList, $answer)";
 	echo "U bent geregistreerd!";
 	}
 
