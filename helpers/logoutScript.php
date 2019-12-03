ada711e163dd7455940686adda2a88b428a586b9
TheEdgyDylan <dylanvanduijnhoven@gmail.com>
2019-12-03 11:36:47 +0100

    idk

--- helpers/logoutScript.php
+++ helpers/logoutScript.php
@@ -2,4 +2,4 @@
 session_start();
 session_unset();
 session_destroy();
-redirect(index);
+redirect('index');
--- helpers/registerScript.php
+++ helpers/registerScript.php
@@ -3,8 +3,10 @@ require("connectionDatabaseScript.php");
 require("antiSQLinjectionscript.php");
 require("redirect.php");
 
+session_start();
+
 error_reporting(E_ERROR | E_PARSE);
-if(!isset($_SESSION["mailadress"])){
+if(!isset($_SESSION["e-mail"])){
 	redirect("register email");
 	exit();
 }
@@ -19,15 +21,15 @@ if(isset($_POST["username"])) {
 	$query = $sql->execute(array([$username]));
 	$users = $query->fetchAll(pdo::FETCH_ASSOC);
 	if(isarray($user)){
-		
-	};
-	if(sqlsrv_nums_row($query) == 0){
+	  echo'Deze gebruikersnaam staat al geregistreerd!';
+	  exit();
+	}
+	/* if(sqlsrv_nums_row($query) == 0){
 		$result = add_customer($_POST);
  		if ($result === true ){	
 			die();
-		}
+		} */
   }
-}	
 /*else {
 	if(isset($_POST["gebruikersnaam"])){
     $existing = existing_customer();
@@ -39,20 +41,20 @@ if(isset($_POST["username"])) {
 /* Geeft de ingevulde waarden bij gebruikersnaam en wachtwoord mee aan de variabelen */
 /* zet het mailadres in een sessie en unset deze nadat je hem in de database hebt gestopt.	*/
 if(isset($_POST["submit"])){
-	$username = $dbh->real_escape_string($_POST(["username"]));
-	$password = $dbh->real_escape_string($_POST(["password"]));
-	$repeatpassword = $dbh->real_escape_string($_POST(["Rpassword"]));
-	$mailaddress = $dbh->real_escape_string($_POST(["mailadress"]));
-	$firstname = $dbh->real_escape_string($_POST["firstName"]));
-	$lastName = $dbh->real_escape_string($_POST["lastName"]));
-	$adress1 = $dbh->real_escape_string($_POST["adress1"]));
-	$adress2 = $dbh->real_escape_string($_POST["adress2"]));
-	$postalCode = $dbh->real_escape_string($_POST["postalCode"]));
-	$city = $dbh->real_escape_string($_POST["city"]));
-	$land = $dbh->real_escape_string($_POST["land"]));
-	$birthDate = $dbh->real_escape_string($_POST["birthDate"]));
-	$questionList = $dbh->real_escape_string($_POST["questionList"]));
-	$answer = $dbh->real_escape_string($_POST["answer"]));
+	$username = $dbh->real_escape_string($_POST["username"]);
+	$password = $dbh->real_escape_string($_POST["password"]);
+	$repeatpassword = $dbh->real_escape_string($_POST["Rpassword"]);
+	$mailaddress = $dbh->real_escape_string($_POST["mailadress"]);
+	$firstname = $dbh->real_escape_string($_POST["firstName"]);
+	$lastName = $dbh->real_escape_string($_POST["lastName"]);
+	$adress1 = $dbh->real_escape_string($_POST["adress1"]);
+	$adress2 = $dbh->real_escape_string($_POST["adress2"]);
+	$postalCode = $dbh->real_escape_string($_POST["postalCode"]);
+	$city = $dbh->real_escape_string($_POST["city"]);
+	$land = $dbh->real_escape_string($_POST["land"]);
+	$birthDate = $dbh->real_escape_string($_POST["birthDate"]);
+	$questionList = $dbh->real_escape_string($_POST["questionList"]);
+	$answer = $dbh->real_escape_string($_POST["answer"]);
 /* Geeft een melding zodra niet alle velden zijn ingevuld */
 try {
 if(empty($username) || empty($password) || empty($repeatpassword) || empty($mailadress)) {
