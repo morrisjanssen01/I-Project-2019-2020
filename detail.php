d9fc65b1380c498df3884c61526939223158cc2f
morrisjanssen01 <desktopmorris@outlook.com>
2019-12-16 11:33:58 +0100

    opnieuw detail grid fout

--- detail.php
+++ detail.php
@@ -18,28 +18,29 @@
                 <?php include 'includes/header.php';?>
             </div>
             <div class="box content">
-                <div class="column1" style="border-right-style:solid; padding:0;">
-                    <div class="row" style="top:0;">
-                            <h1>Title</h1>
-                            <img src="images/rickastley.jpg">
-                            <h3>beschrijving product</h3>
-                            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac aliquam nisl, quis faucibus leo. Aliquam erat volutpat. Morbi non mi molestie, condimentum turpis dictum, dignissim elit. Morbi porttitor egestas sodales. Maecenas sollicitudin aliquet dictum. Phasellus tellus tellus, gravida ut hendrerit a, convallis vel risus. Cras vitae elementum sem. Pellentesque vitae malesuada ex. Maecenas odio leo, suscipit nec quam quis, condimentum facilisis augue. Suspendisse potenti. Quisque non lectus sit amet est porttitor aliquet sit amet et lacus. Duis semper at metus id accumsan. Proin neque metus, ultricies ut leo quis, semper porttitor magna. Nulla vehicula lorem ante.</h5>
-                        </div>
-                    </div>
+                <div class="column1" style="border-right-style:solid; padding:0; width:40%;">
+                    <h1>Title</h1>
+                    <img src="images/rickastley.jpg" style="max-width:100%">
+                    <h3>beschrijving product</h3>
+                    <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac aliquam nisl, quis faucibus leo. Aliquam erat volutpat. Morbi non mi molestie, condimentum turpis dictum, dignissim elit. Morbi porttitor egestas sodales. Maecenas sollicitudin aliquet dictum. Phasellus tellus tellus, gravida ut hendrerit a, convallis vel risus. Cras vitae elementum sem. Pellentesque vitae malesuada ex. Maecenas odio leo, suscipit nec quam quis, condimentum facilisis augue. Suspendisse potenti. Quisque non lectus sit amet est porttitor aliquet sit amet et lacus. Duis semper at metus id accumsan. Proin neque metus, ultricies ut leo quis, semper porttitor magna. Nulla vehicula lorem ante.</h5>
                 </div>
-                <div class="column2">
-                        <div class="col s8">
-                            <h1>veilerinfo<h1>
-                            <div class="row">
-                                <div class="col 7">
-                                    <h3>naam veiler</h3>
-                                </div>
+                <div class="column2 left" style="float:left; display:float;">
+                    <h1>Naam Verkoper</h1>
+                    <h5>Plaatsnaam, Postcode, Land</h5>
+                    <h5>E-mailadres</h5>
+                    <div style="margin-top:30%;">
+                        <h5>looptijd</h5>
+                    </div>
+                    <div style="margin-top:40%;">
+                        <form action="biedScript.php?" method="POST">
+                            <div class="form-field">
+                                <label for="bod">Bod</label>
+                                <input type="text" name="bod" id="bod" placeholder="5.00">
                             </div>
-                            <div class="row">
-                                <div class="col 12">
-                                </div>
+                            <div class="form-field">
+                                <button class="btn-large warmSand darken-2" name="submit" id="submit">Bieden</button>
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
