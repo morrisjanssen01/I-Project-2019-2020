f657936be09dac6c1cb8559eb604e8b1264d74cc
morrisjanssen01 <desktopmorris@outlook.com>
2019-12-17 09:50:28 +0100

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="node_modules/materialize-css/dist/css/materialize.css">
+        <link type="text/css" rel="stylesheet" href="css/stylesheet.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="box header">
+                <?php include 'includes/header.php';?>
+            </div>
+            <div class="box content center">
+                <h1 style="color:red;">404</h1>
+                <div>
+                    <p>Helaas,</p>
+                    <p>Deze pagina bestaat niet.</p>
+                    <p>Probeer het opnieuw of keer terug naar de <a href="index.php">Homepage</a>
+                </div>
+            </div>
+            <div class="box footer">
+             <?php include 'includes/footer.php';?>
+            </div>
+        </div>
+    </body>
+</html>
