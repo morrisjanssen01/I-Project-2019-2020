820b62ae161324f2eee4979211a4ebb04a903a27
EdgyDylan <45945757+EdgyDylan@users.noreply.github.com>
2020-01-08 12:24:59 +0100

    d

--- OfferItem.php
+++ OfferItem.php
@@ -53,94 +53,7 @@ function loadForm(){
             </form>';
             
             }
-
-            //dit moet nog geupdate worden om OfferItem te worden. is nu nog register
-    else if(isset($_SESSION["prevPost"])){
-        echo'
-            <form action="helpers/registerScript.php" method="post">       
-                <div class="form-field">
-                    <label for="username">Gebruikersnaam*</label>
-                    <input type="text" name="username" id="username" pattern="[a-zA-Z0-9_]{1,50}" title="je kunt alleen letter cijfers en de underscore gebruiken in uw gebruikersnaam" value="'.$_SESSION["prevPost"]["username"].'" required>
-                </div>
-                <div class="row"> 
-                    <div class="col s6 form-field">
-                        <label for="password">Wachtwoord*</label>
-                        <input type="password" name ="password" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="het wachtwoord moet een hoofdletter, een kleine letter, een cijfer en minmaal 8 karakters bevatten" required>
-                    </div>
-                    <div class="col s6 form-field">
-                        <label for="repeat password">Bevestig Wachtwoord*</label>
-                        <input type="password" name="repeatpassword" id="repeatpassword" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="het wachtwoord moet een hoofdletter, een kleine letter, een cijfer en minmaal 8 karakters bevatten" required>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col s6 form-field">
-                        <label for="firstName">Voornaam*</label>
-                        <input type="text" name="firstName" id="firstName" value="'.$_SESSION["prevPost"]["firstName"].'" required>
-                    </div>
-                    <div class="col s6 form-field">
-                        <label for="lastName">Achternaam*</label>
-                        <input type="text" name="lastName" id="lastName" value="'.$_SESSION["prevPost"]["lastName"].'" required>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col s6 form-field">
-                        <label for="adress1">Adres 1*</label>
-                        <input type="text" name="adress1" id="adress1" value="'.$_SESSION["prevPost"]["adress1"].'" required>
-                    </div>
-                    <div class="col s6 form-field">
-                        <label for="adress2">Adres 2</label>
-                        <input type="text" name="adress2" id="adress2" value="'.$_SESSION["prevPost"]["adress2"].'">
-                    </div>
-                </div>
-                <div class="row">
-                <div class="col s6 form-field">
-                    <label for="postalCode">Postcode*</label>
-                    <input type="text" name="postalCode" id="postalCode" pattern="[0-9A-Za-z]{6,7}" title="vul een geldige postcode in" value="'.$_SESSION["prevPost"]["postalCode"].'" required>
-                </div>
-                <div class="col s6 form-field">
-                    <label for="city">Plaatsnaam*</label>
-                    <input type="text" name="city" id="city" value="'.$_SESSION["prevPost"]["city"].'" required>
-                </div>
-                </div>
-                <div class="form-field">
-                    <label for="land">Land*</label>
-                    <input type="text" name="land" id="land" value="'.$_SESSION["prevPost"]["land"].'" required>
-                </div>
-                <div class="form-field">
-                    <label for="birthDate">Geboortedag*</label>
-                    <input type="text" name="birthdate" id="birthDate" class="datepicker" pattern="[0-9]{8,8}" value="'.$_SESSION["prevPost"]["birthdate"].'" title="Vul uw geboorte datum in als: JaarMaandDatum" required> 
-                </div>
-                <div class="form-field">
-                    <label for="questionList">Vragen*</label>
-                    <select name="questionList" id="questionList" value="'.$_SESSION["prevPost"]["questionList"].'"required>
-                        <option value="" disabled selected>Kies uw beveiligingsvraag</option>
-                        <option value="1">Hoe heet uw huisdier?</option>
-                        <option value="2">Wat is uw geboorteplaats</option>
-                        <option value="3">Wat is de naam van uw oude basisschool</option>
-                    </select>    
-                </div>
-                <div class="form-field">
-                    <label for="answer">Antwoord*</label>
-                    <input type="text" name="answer" id="answer" value="'.$_SESSION["prevPost"]["answer"].'" required>
-                </div>
-                <div class="form-field">
-                    <label for="verification">verificatiecode*</label>
-                    <input type="text" name="verificationcode" pattern="[0-9]{6,6}" title="De verificatie code is altijd 6 cijfers lang" id="verificationcode" required>
-                </div>
-                <div class="form-field">
-                    <label>
-                        <input type="checkbox" id="remember" required>
-                        <span>Akkoord met Voorwaarden*</span>
-                    </label><br>
-                    <label><span>Velden met * zijn verplicht</span></label>
-                </div>
-                <div class="form-field">
-                    <button class="btn-large warmSand darken-2" name="submit" id="submit">Registreer</button>
-                </div>
-            </form>';
-
-    }
-}
+        }
 ?>
 <!DOCTYPE html>
 <html>
@@ -164,7 +77,7 @@ function loadForm(){
                         <div class="col s12 l6 offset-l3">
                             <div class="card">
                                 <div class="card-action warmSand darken-2 white-text">
-                                    <h3 class="center">Registreren</h3>
+                                    <h3 class="center">Veiling maken</h3>
                                     <!-- maakt de vragenknop aan (verander brown om de kleur te veranderen)-->   
                                     <a class="waves-effect waves-light brown btn modal-trigger"  href="#modal1" > Vragen?</a> 
                                 </div>
@@ -245,4 +158,3 @@ function loadForm(){
         </div>
     </body>
 </html>
-<!-- 2de knop maken die een 2de mail stuurd doormiddel van een knop e-mail niet ontvangen of een knop E-mail binnen 5 min niet ontvangen -->
